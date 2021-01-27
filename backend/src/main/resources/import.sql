7a89a0ee129f3fc0ed6f8b085fa9cb8a817cb6db
TiagoBorborema <tiagoborborema.ti@gmail.com>
2021-01-26 22:27:08 -0300

    Auditing data

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-insert into tb_category(name) values('Books');
-insert into tb_category(name) values('Electronics');
-insert into tb_category(name) values('Computers');
+insert into tb_category(name, createdAt) values('Books', NOW());
+insert into tb_category(name, createdAt) values('Electronics',NOW());
+insert into tb_category(name, createdAt) values('Computers', NOW());
