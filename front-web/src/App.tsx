fc24b2d57a599a79fcbb4c10fa77a0ca17e89ae8
TiagoBorborema <tiagoborborema.ti@gmail.com>
2021-02-13 18:23:59 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import './core/assets/styles/custon.scss';
+import './app.scss';
+import Navbar from './core/components/Navbar'
 const App = ()=>{
    
    return (
-   <h1>App</h1>
-    
+   <Navbar />
    );
 }
 export default App; 
