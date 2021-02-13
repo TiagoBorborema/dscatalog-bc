52cf43d9e2813a9cf396e2287be57c39acc0ae5b
TiagoBorborema <tiagoborborema.ti@gmail.com>
2021-02-13 19:43:18 -0300

    create routes

--- front-web/src/core/components/Navbar/index.tsx
+++ front-web/src/core/components/Navbar/index.tsx
@@ -1,30 +1,33 @@
 import React from 'react';
+import {Link, NavLink} from 'react-router-dom';
 import './styles.scss';
+
+
 const Navbar = () =>(
 
     <nav className="row bg-primary main-nav">
         <div className="col-2">
-            <a href="link" className="nav-logo-text">
+            <Link to="l/" className="nav-logo-text">
             <h4>Ds Catalog</h4>
-            </a>
+            </Link>
         </div>
 
         <div className="col-6 offset-2">
             <ul className="main-menu">
                 <li>
-                    <a href="" className="active">
+                    <NavLink to ="/" activeClassName="active" exact>
                         HOME
-                    </a>
+                    </NavLink>
                 </li>
                 <li>
-                    <a href="">
+                    <NavLink to="/catalog" activeClassName="active">
                         CATALOG
-                    </a>
+                    </NavLink>
                 </li>
                 <li>
-                    <a href="">
+                    <NavLink to="/admin" activeClassName="active">
                         ADMIN
-                    </a>
+                    </NavLink>
                 </li>
             </ul>
 
